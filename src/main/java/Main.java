57af092a9d540d5e7406083cc61b8dfdc0d50955
efixty <efixty@gmail.com>
2018-01-12 19:01:02 +0400

    First commit with .gitignore

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println(sum(a, b));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
