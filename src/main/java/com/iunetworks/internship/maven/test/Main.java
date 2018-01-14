dff46635317d796324d0b3a54100e0f41411691e
efixty <efixty@gmail.com>
2018-01-14 18:06:47 +0400

    reserve/free v.2

--- src/main/java/com/iunetworks/internship/maven/test/Main.java
+++ src/main/java/com/iunetworks/internship/maven/test/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
 	private static BufferedReader reader;
 	private static ArrayList<byte[]> arrayList;
+	private static int input;
 
     public static void main(String[] args) throws Exception {
 
@@ -15,14 +16,29 @@ public class Main {
 
     	while(true) {
 
-    		if(Integer.parseInt(reader.readLine()) == 1) {
-    			arrayList.add(new byte[1024]);
-    			System.out.println("1mb memory was reserved");
-    		} else if(arrayList.size() == 0) System.out.println("Nothing to remove");
-    		else {
-    			arrayList.remove(0);
-    			System.out.println("1mb memory was freed");
+    		switch(Integer.parseInt(reader.readLine())) {
+    			case 1:
+	    			arrayList.add(new byte[1024 * 1024]);
+	    			System.out.println("1mb memory was reserved");
+    				break;
+
+    			case 2:
+    				if(arrayList.size() == 0) 
+    					System.out.println("Nothing to remove");
+    				else {
+    					arrayList.remove(0);
+    					System.out.println("1mb memory was freed");
+    				}
+    				break;
+
+    			case 3: 
+    				System.gc();
+    				System.out.println("GC was called");
+    				break;
+
+    			default: System.out.println("Bad input");
     		}
+
     	}
 
 	}
